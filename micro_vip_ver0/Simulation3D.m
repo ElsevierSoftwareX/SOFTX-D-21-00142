4b5ed9a4e3ff2624a758d9de5bfe6006153d9235
Ali Ahmad <ahmad@eq2-cf-691.creatis.insa-lyon.fr>
2020-10-14 09:17:30 +0200

    Améelleorer la réalisme de simulateur

--- micro_vip_ver0/Simulation3D.m
+++ micro_vip_ver0/Simulation3D.m
@@ -1,7 +1,8 @@
-function [PSF,sim_img,ADN]=Simulation3D(ADN,NA,n,lambda,Nxy,Nz,radius)
+function [img,aotf,psf,ADN]=Simulation3D(ADN,lambda,n,NA,pixelsize,magnification,N,zrange,dz,show)
 
 %% sim_img : image simulée
-%% Refine cell size as cercle of  radius
+% %% Refine cell size as cercle of  radius
+radius=300; 
 D = sqrt(sum((ADN - [0,0,0]).^2, 2));
 p1=ADN(:,1);
 p11=p1(D<radius);
@@ -15,77 +16,89 @@ ADN=[p11 p22 p33];
 
 %% cell rotation : rotation de la nuage 3D autour x, y ou z aléatoirement choisi avec une angle qui est aléatoirment choisi
  angle=randi([0 360]);
- axis=randi([1 3]);
- if axis==1 
- [XYZnew, R, t] = AxelRot(ADN', angle, [1 0 0],[]); 
- end
- if axis==2
-     [XYZnew, R, t] = AxelRot(ADN', angle, [0 1 0],[]); 
- end 
- if axis==3
-    [XYZnew, R, t] = AxelRot(ADN', angle, [0 0 1],[]); 
- end 
+% axis=randi([1 3]);
+ 
+ [XYZnewx, R, t] = AxelRot(ADN', randi([0 360]), [1 0 0],[]); 
+ [XYZnewy, R, t] = AxelRot(XYZnewx, randi([0 360]), [0 1 0],[]); 
+ [XYZnewxyz, R, t] = AxelRot(XYZnewy, randi([0 360]), [0 0 1],[]); 
+
      
-pts=XYZnew';
-
-%% positive 3D PC
-a=min(pts(:,1)); 
-pts(:,1)=pts(:,1)+abs(a)+1; 
-
-a=min(pts(:,2));
-pts(:,2)=pts(:,2)+abs(a)+1;
-
-a=min(pts(:,3));
-pts(:,3)=pts(:,3)+abs(a)+1;
-pts=floor(pts);
+pts=XYZnewxyz'; %% rotated 3D point cloud
 
 
 
-%% interpolation en x
+
+%% %% max interpolation
+%en x
 xmax=max(pts(:,1)); 
-ptsx=(pts(:,1)./xmax).*(floor(Nxy/2)); 
-ptsx=ceil(ptsx);
-%% interpolation en y
+ptsx=(pts(:,1)./xmax).*(5); 
+ptsx=(ptsx);
+% interpolation en y
 ymax=max(pts(:,2)); 
-ptsy=(pts(:,2)./ymax).*(floor(Nxy/2)); 
-ptsy=ceil(ptsy);
+ptsy=(pts(:,2)./ymax).*(5); 
+ptsy=(ptsy);
 
-%% interpolation en z
+% interpolation en z
 zmax=max(pts(:,3)); 
-ptsz=(pts(:,3)./zmax).*Nz; 
-ptsz=ceil(ptsz);
-%% 
+ptsz=(pts(:,3)./zmax).*5; 
+ptsz=(ptsz);
 clear ADN
 ADN=[ptsx ptsy ptsz]; 
+clear ptsx ptsy ptsz
+%% min interpolation
+% en x
+xmin=min(pts(:,1)); 
+ptsx=(pts(:,1)./xmin).*(-5); 
+ptsx=(ptsx);
+% interpolation en y
+ymin=min(pts(:,2)); 
+ptsy=(pts(:,2)./ymin).*(-5); 
+ptsy=(ptsy);
 
-disp(['ADN number is: ', num2str(size(ADN,1))]);
-%% 
-Ib=zeros(Nxy,Nxy,Nz); 
-for it=1:size(ADN,1)
-    
-Ib(ADN(it,1)+floor(Nxy/4),ADN(it,2)+floor(Nxy/4),ADN(it,3))=1;
-end 
+%% interpolation en z
+zmin=min(pts(:,3)); 
+ptsz=(pts(:,3)./zmin).*(-5); 
+ptsz=(ptsz);
+
+clear ADN
+ADN=[ptsx ptsy ptsz];
 
 
+%% modélisation de PSF
+[aotf,otf,psf,Nn,dxn,Nz,Nzn]=model_PSF(lambda,n,NA,pixelsize,magnification,N,zrange,dz,show);
 
-%% Modeling 3D PSF for Widefield 
-dx=0.1; % lateral scaling 
-dz=0.2;
-sigma_x=0.21*(lambda./NA);  % approximation to sigma in lateral
-disp(['Lateral resolution is: ', num2str(sigma_x),' um']);
-sigma_x=sigma_x./dx; 
-sigma_y=sigma_x;
+%% conversion de  la nuages do point dans une image en espace de fourier
+disp('Creating image of 3D point cloud');
+xyrange = Nn/2*dxn;
+dkxy = pi/xyrange;
+kxy = -Nn/2*dkxy:dkxy:(Nn/2-1)*dkxy;
+dkz = pi/zrange;
+kz = -Nzn/2*dkz:dkz:(Nzn/2-1)*dkz;
 
-sigma_z=0.75*(n*lambda)/NA^2; % approximation to sigma in axial
-disp(['Axial resolution is: ', num2str(sigma_z),' um']);
-sigma_z=sigma_z/dz;
+points2img=complex(single(zeros(Nn,Nn,Nzn))); % images des points en espace de fourrier
+pxyz = complex(single(zeros(Nn,Nn,Nzn)));
+      
+for it = 1:size(ADN,1)
+     x=ADN(it,1);
+     y=ADN(it,2);
+     z=ADN(it,3); 
+        
+     px = exp(1i*single(x*kxy));
+     py = exp(1i*single(y*kxy));
+     pz = exp(1i*single(z*kz)); 
+     pxy = px.'*py;
+        
+        for ii = 1:length(kz)
+            pxyz(:,:,ii) = pxy.*pz(ii); 
+        end
+        points2img = points2img+pxyz;
+        
+ end
 
-sigma=[sigma_x sigma_y sigma_z]; % define 3D sigma size
-hsize=[2*ceil(2*sigma_x)+1 2*ceil(2*sigma_y)+1 2*ceil(2*sigma_z)+1]; %  kernel size 
-PSF =fspecial3('gaussian',hsize,sigma); % generate 3D psf
- 
- 
- %% Convolution nuage de point avec 3D PSF
- sim_img = convn(Ib,PSF,'same');  
- 
+ %% Création de l'image microscopique finale: 
+disp('Creating 3D microscopy image');
+img = zeros(N,N,Nz,'single'); 
+ootf = fftshift(otf) .*points2img;
+img = abs(ifftn(ootf,[N N Nz]));  
+        
 end
--- micro_vip_ver0/chain_chrom.m
+++ micro_vip_ver0/chain_chrom.m
@@ -1,27 +1,29 @@
-function Ensoneof=chain_chrom
+function [Ensoneof,ADN]=chain_chrom(d,DC)
+
+
+%% Génération des chaines de chromatine
 round1=1;
 round2=1;
 round3=1;
 p=randperm(100); % vecteur p(1,100) dont les valeurs entre 1 et 100 sont choisies aléatoirement
-for ii=1:64
+for ii=1:DC
      
-if ii<17
-   load(['data_base_chro_chaine/100',num2str(ii),'.mat']);
+
+   load(['data_base_chro_chaine/100',num2str(mod(ii,16)+1),'.mat']);
    Ensoneof{ii}=squeeze(Ensemble(p(ii),:,:)); 
    
-elseif ii>16 && ii<33
-   load(['data_base_chro_chaine/100',num2str(round1),'.mat']);
-   Ensoneof{ii}=squeeze(Ensemble(p(ii),:,:)); 
-  round1=round1+1;
    
-elseif ii>32 && ii< 49 
-   load(['data_base_chro_chaine/100',num2str(round2),'.mat']);
-   Ensoneof{ii}=squeeze(Ensemble(p(ii),:,:)); 
-   round2=round2+1;
-else
-     load(['data_base_chro_chaine/100',num2str(round3),'.mat']);
-      round3=round3+1;
-     Ensoneof{ii}=squeeze(Ensemble(p(ii),:,:));
 end  
+
+
+%% Génération des biomarqueurs selon une distribution uniform
+% Uncomment to visualize
+%%figure(1);
+ADN=[];
+for i=1:length(Ensoneof) 
+  ch=Ensoneof{i};
+   [q] = curvspace_unif(ch,d);
+   ADN=[ADN ; q] ; %%%%%% toutes les coordonées des ADNs générés (IMPORTANT, entrée de MODULE 2)%%%%%%% 
+ end 
 
 end 
